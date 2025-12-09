eb24678d6269c010fc246a5bff6e3bbfce48fc4e
Vanya Dondov <vanyadondov@gmail.com>
2025-12-09 17:31:24 +0200

    lenis and 2nd section added

--- myReact/src/App.tsx
+++ myReact/src/App.tsx
@@ -1,7 +1,9 @@
 import './App.css'
+import { useLenis } from './hooks/useLenis'
 import { AppRouter } from './routers/AppRouter'
 
 function App() {
+  useLenis()
   return <AppRouter />
 }
 
--- myReact/src/pages/register/ui/ArticlesPage.tsx
+++ myReact/src/pages/register/ui/ArticlesPage.tsx
@@ -1,4 +1,3 @@
-import { Link } from 'react-router-dom'
 import './ArcrticlesPage.css'
 
 export default function ArticlesPage() {
